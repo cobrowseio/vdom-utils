6aba298cb1f6ba5798c5ca1ae824bbbf6c9c5943
Andy <andy@coggle.it>
2019-02-28 20:09:01 +0000

    count about:blank as xdoc

--- dist/isxdoc.js
+++ dist/isxdoc.js
@@ -7,10 +7,17 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = isxdoc;
 
+require("core-js/modules/es6.regexp.to-string");
+
+require("core-js/modules/es6.date.to-string");
+
 function isxdoc(node) {
   if (node.tagName !== 'IFRAME') return false;
 
   try {
+    // we count blank iframes as cross document as newly added iframe
+    // will be about:blank even if it's will eventually be a xdoc iframe
+    if (node.contentWindow.location.toString() === 'about:blank') return true;
     return !node.contentWindow.document;
   } catch (e) {
     return true;
--- src/isxdoc.js
+++ src/isxdoc.js
@@ -2,6 +2,9 @@
 export default function isxdoc(node) {
     if (node.tagName !== 'IFRAME') return false;
     try {
+        // we count blank iframes as cross document as newly added iframe
+        // will be about:blank even if it's will eventually be a xdoc iframe
+        if (node.contentWindow.location.toString() === 'about:blank') return true;
         return !node.contentWindow.document;
     } catch (e) {
         return true;
