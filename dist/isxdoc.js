dea252e9ea0137c31bb68d7c02716901b6e7574b
Andy <andy@coggle.it>
2019-05-02 23:41:55 +0100

    support window in isxdoc

--- dist/isxdoc.js
+++ dist/isxdoc.js
@@ -11,15 +11,25 @@ require("core-js/modules/es6.regexp.to-string");
 
 require("core-js/modules/es6.date.to-string");
 
-function isxdoc(node) {
-  if (node.tagName !== 'IFRAME') return false;
-
+function windowIsXdoc(window) {
   try {
-    // we count blank iframes as cross document as newly added iframe
-    // will be about:blank even if it's will eventually be a xdoc iframe
-    if (node.contentWindow.location.toString() === 'about:blank') return true;
-    return !node.contentWindow.document;
+    return !window.document;
   } catch (e) {
     return true;
   }
+}
+
+function isxdoc(node) {
+  if (node instanceof Window) {
+    return windowIsXdoc(node);
+  } else if (node.tagName === 'IFRAME') {
+    if (windowIsXdoc(node.contentWindow)) {
+      return true; // we count blank iframes as cross document as newly added iframe
+      // will be about:blank even if it's will eventually be a xdoc iframe
+    } else if (node.contentWindow.location.toString() === 'about:blank') {
+      return true;
+    }
+  }
+
+  return false;
 }
--- src/isxdoc.js
+++ src/isxdoc.js
@@ -1,12 +1,23 @@
 
-export default function isxdoc(node) {
-    if (node.tagName !== 'IFRAME') return false;
+function windowIsXdoc(window) {
     try {
-        // we count blank iframes as cross document as newly added iframe
-        // will be about:blank even if it's will eventually be a xdoc iframe
-        if (node.contentWindow.location.toString() === 'about:blank') return true;
-        return !node.contentWindow.document;
+        return !window.document;
     } catch (e) {
         return true;
     }
+}
+
+export default function isxdoc(node) {
+    if (node instanceof Window) {
+        return windowIsXdoc(node);
+    } else if (node.tagName === 'IFRAME') {
+        if (windowIsXdoc(node.contentWindow)) {
+            return true;
+        // we count blank iframes as cross document as newly added iframe
+        // will be about:blank even if it's will eventually be a xdoc iframe
+        } else if (node.contentWindow.location.toString() === 'about:blank') {
+            return true;
+        }
+    }
+    return false;
 }
