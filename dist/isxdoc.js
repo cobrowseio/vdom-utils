21ef2a7e2ce88a03a8cb957a1b5e706a893e261d
Andy <andy@coggle.it>
2019-05-10 16:18:30 +0100

    remove blank content assumption

--- dist/isxdoc.js
+++ dist/isxdoc.js
@@ -7,10 +7,6 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = isxdoc;
 
-require("core-js/modules/es6.regexp.to-string");
-
-require("core-js/modules/es6.date.to-string");
-
 function windowIsXdoc(window) {
   try {
     // try to access a property on the window
@@ -25,9 +21,6 @@ function isxdoc(node) {
     return true;
   } else if (node.tagName === 'IFRAME') {
     if (windowIsXdoc(node.contentWindow)) {
-      return true; // we count blank iframes as cross document as newly added iframe
-      // will be about:blank even if it's will eventually be a xdoc iframe
-    } else if (node.contentWindow.location.toString() === 'about:blank') {
       return true;
     }
   }
--- src/isxdoc.js
+++ src/isxdoc.js
@@ -14,10 +14,6 @@ export default function isxdoc(node) {
     } else if (node.tagName === 'IFRAME') {
         if (windowIsXdoc(node.contentWindow)) {
             return true;
-        // we count blank iframes as cross document as newly added iframe
-        // will be about:blank even if it's will eventually be a xdoc iframe
-        } else if (node.contentWindow.location.toString() === 'about:blank') {
-            return true;
         }
     }
     return false;
